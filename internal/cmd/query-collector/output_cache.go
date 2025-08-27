5f77d0505779ecd5ecc153ef5045e4583ce41a65
shfwnz <ilhamdzaky2007@gmail.com>
2025-08-27 16:26:19 +0700

    pref: cache biner

--- internal/cmd/query-collector/output_cache.go
+++ internal/cmd/query-collector/output_cache.go
@@ -3,11 +3,12 @@ package main
 import (
 	"bufio"
 	"context"
+	"encoding/binary"
 	"fmt"
 	"io"
-	"os"
-
 	"mysql-load-test/pkg/query"
+	"os"
+	"sync"
 )
 
 type OutputCacheConfig struct {
@@ -17,9 +18,10 @@ type OutputCacheConfig struct {
 }
 
 type OutputCache struct {
-	cfg     OutputCacheConfig
-	closers []io.Closer
-	writer  *bufio.Writer
+	cfg        OutputCacheConfig
+	closers    []io.Closer
+	writer     *bufio.Writer
+	bufferPool *sync.Pool
 }
 
 func NewCacheOutput(cfg OutputCacheConfig, common *OutputCommon) (*OutputCache, error) {
@@ -30,11 +32,12 @@ func NewCacheOutput(cfg OutputCacheConfig, common *OutputCommon) (*OutputCache, 
 
 	writer, err := common.WrapWriter(file)
 	if err != nil {
+		file.Close()
 		return nil, fmt.Errorf("error wrapping writer: %w", err)
 	}
 	closers := []io.Closer{file}
 
-	bufioWriter := bufio.NewWriter(writer)
+	bufioWriter := bufio.NewWriterSize(writer, 1024*1024)
 
 	if cfg.BatchSize == 0 {
 		cfg.BatchSize = 1000
@@ -44,10 +47,23 @@ func NewCacheOutput(cfg OutputCacheConfig, common *OutputCommon) (*OutputCache, 
 		cfg:     cfg,
 		writer:  bufioWriter,
 		closers: closers,
+		bufferPool: &sync.Pool{
+			New: func() interface{} {
+				b := make([]byte, 4096)
+				return &b
+			},
+		},
 	}, nil
 }
 
 func (o *OutputCache) Destroy() error {
+	if err := o.writer.Flush(); err != nil {
+		for _, closer := range o.closers {
+			closer.Close()
+		}
+		return fmt.Errorf("error flushing buffer: %w", err)
+	}
+
 	for _, closer := range o.closers {
 		if err := closer.Close(); err != nil {
 			return fmt.Errorf("error closing output cache: %w", err)
@@ -64,23 +80,37 @@ func (o *OutputCache) Concurrency() OutputConcurrencyInfo {
 }
 
 func (o *OutputCache) StartOutput(ctx context.Context, inQueryChan <-chan *query.Query) error {
-	var buf []byte
+	defer o.writer.Flush()
+
 	for q := range inQueryChan {
-		querySize := q.GetSize()
-		if buf == nil || len(buf) < querySize {
-			buf = make([]byte, querySize)
+		if q == nil || len(q.Raw) == 0 {
+			continue
 		}
-		var err error
-		var n int
-		n, err = q.MarshalBinary(buf)
-		if err != nil {
-			return fmt.Errorf("error marshaling query: %w", err)
+
+		queryLength := uint32(len(q.Raw))
+		totalSize := 4 + len(q.Raw) + 8
+
+		bufPtr := o.bufferPool.Get().(*[]byte)
+		buf := *bufPtr
+		defer o.bufferPool.Put(bufPtr)
+
+		if cap(buf) < totalSize {
+			buf = make([]byte, totalSize)
+			*bufPtr = buf
 		}
-		// buf[n] = '\n'
-		if _, err := o.writer.Write(buf[:n]); err != nil {
-			return fmt.Errorf("error writing lines: %w", err)
+		buf = buf[:totalSize]
+
+		binary.LittleEndian.PutUint32(buf[0:4], queryLength)
+
+		copy(buf[4:], q.Raw)
+
+		offset := 4 + len(q.Raw)
+		binary.LittleEndian.PutUint64(buf[offset:], q.FingerprintHash)
+
+		if _, err := o.writer.Write(buf); err != nil {
+			return fmt.Errorf("error writing query data: %w", err)
 		}
-		// fmt.Println(string(buf[:n]))
 	}
+
 	return nil
 }
